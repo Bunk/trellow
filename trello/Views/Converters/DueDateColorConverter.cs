73e39a565605b951bdb55504873d4e4da49a4d97
JD Courtoy <jcourtoy@pressganey.com>
2013-02-04 16:15:07 -0600

    Due dates now color code their backgrounds based on the current date.

---
+++ trello/Views/Converters/DueDateColorConverter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace trello.Views.Converters
+{
+    public class DueDateColorConverter : IValueConverter
+    {
+        public SolidColorBrush DefaultBrush { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return DefaultBrush ?? new SolidColorBrush(FromKnownColor("Green"));
+
+            var strVal = value.ToString();
+            var nullable = value as DateTime?;
+            if (nullable != null)
+                strVal = nullable.Value.ToString(CultureInfo.InvariantCulture);
+
+            DateTime dateval;
+            if (DateTime.TryParse(strVal, out dateval))
+            {
+                var given = ((DateTime) value).ToLocalTime();
+                var current = DateTime.Now;
+
+                var difference = given - current;
+
+                if (difference.Ticks < 0)
+                    return new SolidColorBrush(FromKnownColor("Red"));//FromHexa("#ffa82424"));
+
+                if (difference.TotalHours <= 24)
+                    return new SolidColorBrush(FromKnownColor("Orange"));//FromHexa("#ffc3b622"));
+            }
+            return DefaultBrush ?? new SolidColorBrush(FromKnownColor("Green"));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        private Color FromHexa(string hex)
+        {
+            return Color.FromArgb(
+                System.Convert.ToByte(hex.Substring(1, 2), 16),
+                System.Convert.ToByte(hex.Substring(3, 2), 16),
+                System.Convert.ToByte(hex.Substring(5, 2), 16),
+                System.Convert.ToByte(hex.Substring(7, 2), 16));
+        }
+
+        private Color FromKnownColor(string color)
+        {
+            return (Color) XamlReader.Load(
+                @"<Color xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">"
+                + color + "</Color>");
+        }
+    }
+}
