167194f4c0e0a505d6743e945e9e33403c3b7326
JD Courtoy <jcourtoy@pressganey.com>
2013-02-11 23:38:52 -0600

    First attempt at a drop shadow text block.

---
+++ trello/Views/Converters/LabelColorConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace trello.Views.Converters
+{
+    public class LabelColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+//            var color = value.ToString().ToLowerInvariant();
+//            switch (color)
+//            {
+//                case "red":
+//                    break;
+//            }
+            throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
